e40d8fec0c8e585b5217e5923ee7d56d562a667a
Ben-Hur Batista <benhur.onrails@gmail.com>
2014-05-02 13:56:29 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PubliqVida::Application.config.secret_key_base = '72a07d20913277acc46e7b474fbba2860367f00b643adf874e3569ada543210bcc7db41c49f4db0f29c87a2b58b43477c0c5193f2a4db74cfb85155e73cc22f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PubliqVida::Application.config.session_store :cookie_store, key: '_publiq-vida_session'
